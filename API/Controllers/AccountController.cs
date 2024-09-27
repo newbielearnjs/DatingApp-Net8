df14aaf930477e2d97342d7f4ab6626f96c51db3
TUAN NAM TRAN <103792643@student.swin.edu.au>
2024-09-27 11:31:54 +0700

    End of Section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -42,8 +42,10 @@ public class AccountController(DataContext context, ITokenService tokenService) 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await context.AppUsers.FirstOrDefaultAsync(x =>
-            x.UserName == loginDto.Username.ToLower());
+        var user = await context.AppUsers
+            .Include(p => p.Photos)
+            .FirstOrDefaultAsync(x =>
+                x.UserName == loginDto.Username.ToLower());
         if (user == null) return Unauthorized("Invalid username");
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
@@ -56,7 +58,8 @@ public class AccountController(DataContext context, ITokenService tokenService) 
         return new UserDto
         {
             Username = user.UserName,
-            Token = tokenService.CreateToken(user)
+            Token = tokenService.CreateToken(user),
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
         };
     }
 
