87d379557af617d567c84020a6c5b682af6d37f1
TUAN NAM TRAN <103792643@student.swin.edu.au>
2024-10-08 11:46:25 +0700

    End of Section 15

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -5,15 +5,17 @@ namespace API.Extensions;
 
 public static class ClaimsPrincipleExtensions
 {
-    public static string GetUsername(this ClaimsPrincipal user){
-        var username = user.FindFirstValue(ClaimTypes.Name) 
+    public static string GetUsername(this ClaimsPrincipal user)
+    {
+        var username = user.FindFirstValue(ClaimTypes.Name)
             ?? throw new Exception("Cannot get username from token");
 
         return username;
     }
 
-    public static int GetUserId(this ClaimsPrincipal user){
-        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) 
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)
             ?? throw new Exception("Cannot get username from token"));
 
         return userId;
