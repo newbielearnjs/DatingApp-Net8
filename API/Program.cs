d0c89c45136db97ed30708b655609b17ae9bb335
TUAN NAM TRAN <103792643@student.swin.edu.au>
2024-10-21 13:33:50 +0700

    Pre continuous integration

--- API/Program.cs
+++ API/Program.cs
@@ -24,9 +24,13 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 
 using var scope = app.Services.CreateScope();
@@ -37,7 +41,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>(); 
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DETELE FROM [Connections]");
+    await context.Database.ExecuteSqlRawAsync("DELETE FROM Connections");
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
